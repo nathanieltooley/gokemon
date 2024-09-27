2f3b10a07142f6e559715407eeca1db13ecd7944
nathanieltooley <gotablast99@gmail.com>
2024-09-26 23:50:00 -0500

    Add basic views for pokemon selection

--- client/views/pokeSelection/pokeSelection.go
+++ client/views/pokeSelection/pokeSelection.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -13,13 +14,36 @@ import (
 var (
 	infoStyle       = lipgloss.NewStyle().Border(lipgloss.NormalBorder(), true).Margin(2).Width(30)
 	infoHeaderStyle = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#FAFAFA")).Border(lipgloss.NormalBorder(), false, false, true).Width(30).Align(lipgloss.Center)
+
+	unselectedEditorStyle = lipgloss.NewStyle().Margin(2)
+	selectedEditorStyle   = lipgloss.NewStyle().Margin(2).Bold(true).Border(lipgloss.BlockBorder(), true)
+)
+
+var (
+	selectEditorLeft = key.NewBinding(
+		key.WithKeys("editorLeft", "h"),
+	)
+
+	selectEditorRight = key.NewBinding(
+		key.WithKeys("editorRight", "l"),
+	)
+)
+
+var editors = [...]string{"Details", "Moves", "Item", "Ability", "EV/IV"}
+
+const (
+	MODE_ADDPOKE = iota
+	MODE_EDITPOKE
 )
 
 type SelectionModel struct {
 	Team   []*game.Pokemon
 	Choice *game.BasePokemon
 
-	list list.Model
+	list                list.Model
+	mode                int
+	currentPokemonIndex int
+	currentEditor       int
 }
 
 func NewModel(pokemon game.PokemonRegistry) SelectionModel {
@@ -42,26 +66,192 @@ func (m SelectionModel) Init() tea.Cmd {
 }
 
 func (m SelectionModel) View() string {
-	var body string
-	header := "Pokemon Selection"
-	if m.Choice != nil {
-		body = fmt.Sprintf("Hp: %d\nAttack: %d\nDef: %d\nSpAttack: %d\nSpDef: %d\nSpeed: %d\n", m.Choice.Hp, m.Choice.Attack, m.Choice.Def, m.Choice.SpAttack, m.Choice.SpDef, m.Choice.Speed)
-		header = fmt.Sprintf("Pokemon: %s", m.Choice.Name)
-	} else {
-		body = ""
+	if m.mode == MODE_ADDPOKE {
+		var body string
+		var header string
+
+		if m.Choice != nil {
+			body = fmt.Sprintf("Hp: %d\nAttack: %d\nDef: %d\nSpAttack: %d\nSpDef: %d\nSpeed: %d\n",
+				m.Choice.Hp,
+				m.Choice.Attack,
+				m.Choice.Def,
+				m.Choice.SpAttack,
+				m.Choice.SpDef,
+				m.Choice.Speed)
+			header = fmt.Sprintf("Pokemon: %s", m.Choice.Name)
+		} else {
+			body = ""
+		}
+
+		dialog := lipgloss.JoinVertical(lipgloss.Left, infoHeaderStyle.Render(header), body)
+		selection := lipgloss.JoinVertical(lipgloss.Center, infoStyle.Render(dialog), m.list.View())
+
+		teamPanels := make([]string, 0)
+
+		for _, pokemon := range m.Team {
+			panel := fmt.Sprintf(`
+                %s
+                Level: %d
+                `, pokemon.Base.Name, pokemon.Level)
+			teamPanels = append(teamPanels, panel)
+		}
+
+		teamView := lipgloss.JoinVertical(lipgloss.Center, teamPanels...)
+
+		return lipgloss.JoinHorizontal(lipgloss.Center, selection, teamView)
+	} else if m.mode == MODE_EDITPOKE {
+		// header := "Editing Pokemon"
+		// var body string
+
+		currentPokemon := m.Team[m.currentPokemonIndex]
+
+		type1 := currentPokemon.Base.Type1.Name
+		type2 := ""
+
+		if currentPokemon.Base.Type2 != nil {
+			type2 = currentPokemon.Base.Type2.Name
+		}
+
+		// TODO: Constant panel showing Pokemon Info
+		info := fmt.Sprintf(`
+            Name: %s
+            Level: %d
+            HP: %d:%d:%d
+            Attack: %d:%d:%d
+            Defense: %d:%d:%d
+            Special Attack: %d:%d:%d
+            Special Defense: %d:%d:%d
+            Speed: %d:%d:%d
+            Type: %s | %s
+            Ability: %s
+            Item: %s
+
+            Move 1: %s
+            Move 2: %s
+            Move 3: %s
+            Move 4: %s
+            `,
+			currentPokemon.Base.Name,
+			currentPokemon.Level,
+
+			currentPokemon.Hp.Value,
+			currentPokemon.Hp.Iv,
+			currentPokemon.Hp.Ev,
+
+			currentPokemon.Attack.Value,
+			currentPokemon.Attack.Iv,
+			currentPokemon.Attack.Ev,
+
+			currentPokemon.Def.Value,
+			currentPokemon.Def.Iv,
+			currentPokemon.Def.Ev,
+
+			currentPokemon.SpAttack.Value,
+			currentPokemon.SpAttack.Iv,
+			currentPokemon.SpAttack.Ev,
+
+			currentPokemon.SpDef.Value,
+			currentPokemon.SpDef.Iv,
+			currentPokemon.SpDef.Ev,
+
+			currentPokemon.Speed.Value,
+			currentPokemon.Speed.Iv,
+			currentPokemon.Speed.Ev,
+
+			type1,
+			type2,
+			"",
+			"",
+
+			"",
+			"",
+			"",
+			"",
+		)
+
+		var newEditors [len(editors)]string
+		// editorTabs := strings.Builder{}
+
+		for i, editor := range editors {
+			if i == int(m.currentEditor) {
+				newEditors[i] = selectedEditorStyle.Render(editor + "\t")
+			} else {
+				newEditors[i] = unselectedEditorStyle.Render(editor + "\t")
+			}
+		}
+
+		tabs := lipgloss.JoinHorizontal(lipgloss.Center, newEditors[0:]...)
+		return lipgloss.JoinVertical(lipgloss.Center, info, tabs)
+
+		// TODO: EV / IV editor
+
+		// TODO: Move editor
+
+		// TODO: Ability editor
+
+		// TODO: Item editor
+
+		// TODO: Level, Name, (maybe tera?) editor
+
+		// TODO: Pokemon Replacement panel?
 	}
 
-	dialog := lipgloss.JoinVertical(lipgloss.Left, infoHeaderStyle.Render(header), body)
+	return ""
 
-	return lipgloss.JoinVertical(lipgloss.Center, infoStyle.Render(dialog), m.list.View())
 }
 
 func (m SelectionModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
+	cmd = nil
 
-	m.list, cmd = m.list.Update(msg)
-	choice, _ := m.list.Items()[m.list.Index()].(item)
-	m.Choice = choice.BasePokemon
+	if m.mode == MODE_ADDPOKE {
+		m.list, cmd = m.list.Update(msg)
+		choice, _ := m.list.Items()[m.list.Index()].(item)
+
+		m.Choice = choice.BasePokemon
+	}
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEnter:
+			if m.mode == MODE_ADDPOKE {
+				if m.Choice != nil && len(m.Team) < 6 {
+					newPokemon := game.NewPokeBuilder(m.Choice).Build()
+					m.Team = append(m.Team, newPokemon)
+				}
+
+				m.currentPokemonIndex = len(m.Team) - 1
+				m.mode = MODE_EDITPOKE
+			}
+		case tea.KeyEscape:
+			if m.mode == MODE_EDITPOKE {
+				m.mode = MODE_ADDPOKE
+			}
+
+		case tea.KeyCtrlC:
+			cmd = tea.Quit
+
+		}
+
+		if m.mode == MODE_EDITPOKE {
+			if key.Matches(msg, selectEditorLeft) {
+				m.currentEditor--
+
+				if m.currentEditor < 0 {
+					m.currentEditor = len(editors) - 1
+				}
+			}
+
+			if key.Matches(msg, selectEditorRight) {
+				m.currentEditor++
+
+				if m.currentEditor >= len(editors) {
+					m.currentEditor = 0
+				}
+			}
+		}
+	}
 
 	return m, cmd
 }
