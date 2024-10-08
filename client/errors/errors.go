61acd0a9812ba9a3e48baa54097385b5d93396a3
nathanieltooley <gotablast99@gmail.com>
2024-10-07 23:22:01 -0500

    Doc formatting

--- client/errors/errors.go
+++ client/errors/errors.go
@@ -1,7 +1,7 @@
 package errors
 
-// Returns the value passed in if there is no error, otherwise it will panic
-// Very similar to Rust's Unwrap method on an Option or Err enum
+// Returns the value passed in if there is no error, otherwise it will panic.
+// Very similar to Rust's Unwrap method on an Option or Err enum.
 func Must[T any](value T, err error) T {
 	if err != nil {
 		panic(err)
