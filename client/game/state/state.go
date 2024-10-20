b3d12d97b982dd6a86f2052dcc9fd186661ea0d6
nathanieltooley <gotablast99@gmail.com>
2024-10-19 19:44:35 -0500

    refactor: move state to its own package
    Did this to avoid a dependency loop with global

---
+++ client/game/state/state.go
@@ -0,0 +1,78 @@
+package state
+
+import (
+	"github.com/nathanieltooley/gokemon/client/game"
+	"github.com/nathanieltooley/gokemon/client/global"
+)
+
+const (
+	HOST = iota
+	PEER
+)
+
+type GameState struct {
+	localPlayer    Player
+	opposingPlayer Player
+	turn           int
+
+	// HOST or PEER
+	// The HOST state is the arbiter of truth
+	// and the PEER state is the replicated state of the HOST
+	stateType int
+}
+
+func NewState() GameState {
+	// For testing purposes only
+	var defaultTeam [6]*game.Pokemon
+	defaultTeam[0] = game.NewPokeBuilder(global.POKEMON.GetPokemonByPokedex(1)).Build()
+	defaultTeam[1] = game.NewPokeBuilder(global.POKEMON.GetPokemonByPokedex(2)).Build()
+
+	localPlayer := Player{
+		Name: "Local",
+		Team: defaultTeam,
+	}
+	opposingPlayer := Player{
+		Name: "Opponent",
+		Team: defaultTeam,
+	}
+
+	return GameState{
+		localPlayer:    localPlayer,
+		opposingPlayer: opposingPlayer,
+		turn:           HOST,
+		stateType:      HOST,
+	}
+}
+
+func (g *GameState) GetPlayer(index int) *Player {
+	if index == HOST {
+		return &g.localPlayer
+	} else {
+		return &g.opposingPlayer
+	}
+}
+
+func (g *GameState) Update(action Action) {
+	action.UpdateState(g)
+}
+
+type Player struct {
+	Name            string
+	Team            [6]*game.Pokemon
+	ActivePokeIndex uint8
+}
+
+type Action interface {
+	// Updates the state using a pointer, based on what type of action it is
+	UpdateState(*GameState)
+}
+
+type SwitchAction struct {
+	PlayerIndex int
+	SwitchIndex int
+}
+
+func (a SwitchAction) UpdateState(state *GameState) {
+	player := state.GetPlayer(a.PlayerIndex)
+	player.ActivePokeIndex = uint8(a.SwitchIndex)
+}
