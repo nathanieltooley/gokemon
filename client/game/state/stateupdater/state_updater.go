0c4681751b7912a08f96ab76d7efe4a8a40bc00b
Nathaniel Tooley <gotablast99@gmail.com>
2025-01-12 01:11:31 -0600

    feat: better ability text

--- client/game/state/stateupdater/state_updater.go
+++ client/game/state/stateupdater/state_updater.go
@@ -2,6 +2,7 @@ package stateupdater
 
 import (
 	"cmp"
+	"fmt"
 	"slices"
 	"time"
 
@@ -403,13 +404,18 @@ func (u *LocalUpdater) SendAction(action state.Action) {
 func endOfTurnAbilities(gameState *state.GameState, player int) []state.StateSnapshot {
 	playerPokemon := gameState.GetPlayer(player).GetActivePokemon()
 
+	states := make([]state.StateSnapshot, 0)
+
+	abilityText := fmt.Sprintf("%s activated their ability: %s", playerPokemon.Nickname, playerPokemon.Ability)
+
 	switch playerPokemon.Ability {
 	// TEST: no gen 1 pkm have this ability
 	case "speed-boost":
-		return []state.StateSnapshot{state.StatChangeHandler(gameState, playerPokemon, game.StatChange{Change: 1, StatName: "speed"}, 100)}
+		states = append(states, state.NewMessageOnlySnapshot(abilityText))
+		states = append(states, state.StatChangeHandler(gameState, playerPokemon, game.StatChange{Change: 1, StatName: "speed"}, 100))
 	}
 
-	return []state.StateSnapshot{state.NewEmptyStateSnapshot()}
+	return states
 }
 
 type (
