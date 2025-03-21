8df176a9309687b0751ea004035e2bba92253410
Nathaniel Tooley <gotablast99@gmail.com>
2025-03-21 00:47:47 -0500

    feat: can now back out of play selection menu

--- client/views/mainmenu/play_selection.go
+++ client/views/mainmenu/play_selection.go
@@ -8,7 +8,8 @@ import (
 )
 
 type PlaySelection struct {
-	buttons components.MenuButtons
+	buttons   components.MenuButtons
+	backtrack components.Breadcrumbs
 }
 
 func NewPlaySelection(backtrack components.Breadcrumbs) PlaySelection {
@@ -37,16 +38,27 @@ func NewPlaySelection(backtrack components.Breadcrumbs) PlaySelection {
 	}
 
 	return PlaySelection{
-		buttons: components.NewMenuButton(buttons),
+		buttons:   components.NewMenuButton(buttons),
+		backtrack: backtrack,
 	}
 }
 
 func (m PlaySelection) Init() tea.Cmd { return nil }
 func (m PlaySelection) View() string  { return rendering.GlobalCenter(m.buttons.View()) }
 func (m PlaySelection) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.Type == tea.KeyEscape {
+			return m.backtrack.PopDefault(func() tea.Model {
+				return NewPlaySelection(m.backtrack)
+			}), nil
+		}
+	}
+
 	newModel, startCmd := m.buttons.Update(msg)
 	if newModel != nil {
 		return newModel, startCmd
 	}
+
 	return m, nil
 }
