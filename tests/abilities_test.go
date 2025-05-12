e544c139860a768b7dc23d6db723942524ffe2c1
Nathaniel Tooley <gotablast99@gmail.com>
2025-05-11 23:54:14 -0500

    test: add drizzle test

---
+++ tests/abilities_test.go
@@ -0,0 +1,29 @@
+package tests
+
+import (
+	"os"
+	"testing"
+
+	"github.com/nathanieltooley/gokemon/client/game"
+	"github.com/nathanieltooley/gokemon/client/game/state"
+	"github.com/nathanieltooley/gokemon/client/game/state/stateupdater"
+	"github.com/nathanieltooley/gokemon/client/global"
+)
+
+func init() {
+	global.GlobalInit(os.DirFS("../"))
+}
+
+func TestDrizzle(t *testing.T) {
+	pokemon := game.NewPokeBuilder(global.POKEMON.GetPokemonByName("bulbasaur")).Build()
+	enemyPokemon := game.NewPokeBuilder(global.POKEMON.GetPokemonByPokedex(2)).Build()
+
+	pokemon.Ability.Name = "drizzle"
+
+	gameState := state.NewState([]game.Pokemon{pokemon}, []game.Pokemon{enemyPokemon})
+	_ = stateupdater.LocalUpdater(&gameState, []state.Action{state.NewSwitchAction(&gameState, state.HOST, 0)})
+
+	if gameState.Weather != game.WEATHER_RAIN {
+		t.Fatalf("Expected weather to be rain: got %d", gameState.Weather)
+	}
+}
