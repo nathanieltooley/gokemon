2ac1ef9881f5a03212db8652f55bb2f84dc1494a
Nathaniel Tooley <gotablast99@gmail.com>
2025-05-11 23:35:48 -0500

    refactor: split up pokemon.go into damage and poke_builder

---
+++ client/game/damage.go
@@ -0,0 +1,147 @@
+package game
+
+import (
+	"math"
+	"math/rand/v2"
+)
+
+func Damage(attacker Pokemon, defendent Pokemon, move Move, crit bool, weather int) uint {
+	attackerLevel := attacker.Level // TODO: Add exception for Beat Up
+	var baseA, baseD uint
+	var a, d uint // TODO: Add exception for Beat Up
+	var aBoost, dBoost int
+
+	if attacker.Ability.Name == "huge-power" {
+		attacker.Attack.RawValue *= 2
+	}
+
+	// Determine damage type
+	switch move.DamageClass {
+	case DAMAGETYPE_PHYSICAL:
+		baseA = attacker.Attack.RawValue
+		a = uint(attacker.Attack.CalcValue())
+		aBoost = attacker.Attack.Stage
+
+		baseD = defendent.Def.RawValue
+		d = uint(defendent.Def.CalcValue())
+		dBoost = defendent.Def.Stage
+	case DAMAGETYPE_SPECIAL:
+		baseA = attacker.SpAttack.RawValue
+		a = uint(attacker.SpAttack.CalcValue())
+		aBoost = attacker.SpAttack.Stage
+
+		baseD = defendent.SpDef.RawValue
+		d = uint(defendent.SpDef.CalcValue())
+		dBoost = defendent.SpDef.Stage
+	}
+
+	power := move.Power
+
+	if power == 0 {
+		return 0
+	}
+
+	damageLogger().Debug().Msgf("Type 1: %#v", defendent.Base.Type1)
+	damageLogger().Debug().Msgf("Type 2: %#v", defendent.Base.Type2)
+
+	attackType := GetAttackTypeMapping(move.Type)
+
+	var type1Effectiveness float32 = 1
+	var type2Effectiveness float32 = 1
+
+	if attackType != nil {
+		type1Effectiveness = attackType.AttackEffectiveness(defendent.Base.Type1.Name)
+
+		if defendent.Base.Type2 != nil {
+			type2Effectiveness = attackType.AttackEffectiveness(defendent.Base.Type2.Name)
+		}
+	}
+
+	if type1Effectiveness == 0 || type2Effectiveness == 0 {
+		return 0
+	}
+
+	if defendent.Ability.Name == "wonder_guard" {
+		if type1Effectiveness <= 1 && type2Effectiveness <= 1 {
+			return 0
+		}
+	}
+
+	var critBoost float32 = 1
+	if crit && defendent.Ability.Name != "battle-armor" && defendent.Ability.Name != "shell-armor" {
+		critBoost = 1.5
+		a = baseA
+		d = baseD
+
+	} else if crit && (defendent.Ability.Name == "battle-armor" || defendent.Ability.Name == "shell-armor") {
+		damageLogger().Info().Msgf("Defending pokemon blocked crits with %s", defendent.Ability.Name)
+	}
+
+	var lowHealthBonus float32 = 1.0
+	if float32(attacker.Hp.Value) <= float32(attacker.MaxHp)*0.33 {
+		if (attacker.Ability.Name == "overgrow" && move.Type == TYPENAME_GRASS) ||
+			(attacker.Ability.Name == "blaze" && move.Type == TYPENAME_FIRE) ||
+			(attacker.Ability.Name == "torrent" && move.Type == TYPENAME_WATER) ||
+			(attacker.Ability.Name == "swarm" && move.Type == TYPENAME_BUG) {
+			lowHealthBonus = 1.5
+		}
+	}
+
+	a = uint(float32(a) * lowHealthBonus)
+
+	// Calculate the part of the damage function in brackets
+	damageInner := ((((float32(2*attackerLevel) / 5) + 2) * float32(power) * (float32(a) / float32(d))) / 50) + 2
+	randomSpread := float32(rand.UintN(15)+85) / 100
+	var stab float32 = 1
+
+	if move.Type == attacker.Base.Type1.Name || (attacker.Base.Type2 != nil && move.Type == attacker.Base.Type2.Name) {
+		stab = 1.5
+	}
+
+	var burn float32 = 1
+	// TODO: Add exception for Guts and Facade
+	if attacker.Status == STATUS_BURN && move.DamageClass == DAMAGETYPE_PHYSICAL {
+		burn = 0.5
+		damageLogger().Info().Float32("burn", burn).Msg("Attacker is burned and is using a physical move")
+	}
+
+	// TODO: Maybe add weather
+	var weatherBonus float32 = 1.0
+	if (weather == WEATHER_RAIN && move.Type == TYPENAME_WATER) || (weather == WEATHER_SUN && move.Type == TYPENAME_FIRE) {
+		weatherBonus = 1.5
+	}
+	if (weather == WEATHER_RAIN && move.Type == TYPENAME_FIRE) || (weather == WEATHER_SUN && move.Type == TYPENAME_WATER) {
+		weatherBonus = 0.5
+	}
+
+	// TODO: Maybe check for parental bond, glaive rush, targets in DBs, ZMoves
+
+	// TODO: There are about 20 different moves, abilities, and items that have some sort of
+	// random effect to the damage calcs. Maybe implement the most impactful ones?
+
+	// This seems to mostly work, however there are issues when it comes to rounding
+	// and it seems that the lowest possible value in a damage range may not be able
+	// to show up as often because rounding is a bit different
+	// TODO: maybe make a custom rounding function that rounds DOWN at .5
+	damage := uint(math.Round(float64(damageInner * randomSpread * type1Effectiveness * type2Effectiveness * stab * burn * critBoost * weatherBonus)))
+
+	damageLogger().Debug().
+		Int("power", power).
+		Uint("attackerLevel", attackerLevel).
+		Uint("attackValue", a).
+		Int("attackChange", aBoost).
+		Uint("defValue", d).
+		Int("defenseChange", dBoost).
+		Str("attackType", move.Type).
+		Float32("lowHealthBonus", lowHealthBonus).
+		Float32("damageInner", damageInner).
+		Float32("randomSpread", randomSpread).
+		Float32("STAB", stab).
+		Float32("Net Type Effectiveness", type1Effectiveness*type2Effectiveness).
+		Float32("crit", critBoost).
+		Float32("weatherBonus", weatherBonus).
+		Uint("damage", damage).
+		Msg("damage calc")
+
+	return damage
+}
---
+++ client/game/poke_builder.go
@@ -0,0 +1,211 @@
+package game
+
+import (
+	"math"
+	"math/rand/v2"
+
+	"github.com/samber/lo"
+)
+
+type PokemonBuilder struct {
+	poke Pokemon
+}
+
+func NewPokeBuilder(base *BasePokemon) *PokemonBuilder {
+	poke := Pokemon{
+		Base:     base,
+		Nickname: base.Name,
+		Level:    1,
+		Hp:       HpStat{0, 0, 0},
+		Attack:   Stat{0, 0, 0, 0},
+		Def:      Stat{0, 0, 0, 0},
+		SpAttack: Stat{0, 0, 0, 0},
+		SpDef:    Stat{0, 0, 0, 0},
+		RawSpeed: Stat{0, 0, 0, 0},
+		Nature:   NATURE_HARDY,
+	}
+
+	return &PokemonBuilder{poke}
+}
+
+func (pb *PokemonBuilder) SetEvs(evs [6]uint) *PokemonBuilder {
+	pb.poke.Hp.Ev = evs[0]
+	pb.poke.Attack.Ev = evs[1]
+	pb.poke.Def.Ev = evs[2]
+	pb.poke.SpAttack.Ev = evs[3]
+	pb.poke.SpDef.Ev = evs[4]
+	pb.poke.RawSpeed.Ev = evs[5]
+
+	builderLogger().Debug().
+		Uint("HP", evs[0]).
+		Uint("ATTACK", evs[1]).
+		Uint("DEF", evs[2]).
+		Uint("SPATTACK", evs[3]).
+		Uint("SPDEF", evs[4]).
+		Uint("SPEED", evs[5]).Msg("Setting EVs")
+
+	return pb
+}
+
+func (pb *PokemonBuilder) SetIvs(ivs [6]uint) *PokemonBuilder {
+	pb.poke.Hp.Iv = ivs[0]
+	pb.poke.Attack.Iv = ivs[1]
+	pb.poke.Def.Iv = ivs[2]
+	pb.poke.SpAttack.Iv = ivs[3]
+	pb.poke.SpDef.Iv = ivs[4]
+	pb.poke.RawSpeed.Iv = ivs[5]
+
+	builderLogger().Debug().
+		Uint("HP", ivs[0]).
+		Uint("ATTACK", ivs[1]).
+		Uint("DEF", ivs[2]).
+		Uint("SPATTACK", ivs[3]).
+		Uint("SPDEF", ivs[4]).
+		Uint("SPEED", ivs[5]).Msg("Setting IVs")
+
+	return pb
+}
+
+func (pb *PokemonBuilder) SetPerfectIvs() *PokemonBuilder {
+	pb.poke.Hp.Iv = MAX_IV
+	pb.poke.Attack.Iv = MAX_IV
+	pb.poke.Def.Iv = MAX_IV
+	pb.poke.SpAttack.Iv = MAX_IV
+	pb.poke.SpDef.Iv = MAX_IV
+	pb.poke.RawSpeed.Iv = MAX_IV
+
+	builderLogger().Debug().Msg("Setting Perfect IVS")
+
+	return pb
+}
+
+func (pb *PokemonBuilder) SetRandomIvs() *PokemonBuilder {
+	var ivs [6]uint
+
+	for i := range ivs {
+		iv := rand.UintN(MAX_IV + 1)
+		ivs[i] = iv
+	}
+
+	builderLogger().Debug().Msg("Setting Random IVs")
+	pb.SetIvs(ivs)
+
+	return pb
+}
+
+// Returns an array of EV spreads whose total == 510
+// and follow the order of HP, ATTACK, DEF, SPATTACK, SPDEF, SPEED
+// TODO: pretty sure this doesn't work
+func (pb *PokemonBuilder) SetRandomEvs() *PokemonBuilder {
+	evPool := MAX_TOTAL_EV
+	var evs [6]uint
+
+	for evPool > 0 {
+		// randomly select a stat to add EVs to
+		randomIndex := rand.UintN(6)
+		currentEv := evs[randomIndex]
+
+		remainingEvSpace := MAX_EV - currentEv
+
+		if remainingEvSpace <= 0 {
+			continue
+		}
+
+		// Get a random value to increase the EV by
+		// ranges from 0 to (remainingEvSpace or MAX_EV) + 1
+		randomEv := rand.UintN(uint(math.Max(float64(remainingEvSpace), MAX_EV)) + 1)
+		evs[randomIndex] += randomEv
+		evPool -= int(randomEv)
+	}
+
+	builderLogger().Debug().Msg("Setting Random EVs")
+	pb.SetEvs(evs)
+
+	builderLogger().Debug().Msgf("EV Total: %d", pb.poke.GetCurrentEvTotal())
+	return pb
+}
+
+func (pb *PokemonBuilder) SetLevel(level uint) *PokemonBuilder {
+	pb.poke.Level = level
+	return pb
+}
+
+func (pb *PokemonBuilder) SetRandomLevel(low int, high int) *PokemonBuilder {
+	n := uint(high - low)
+	rndLevel := rand.UintN(n) + uint(low)
+	pb.poke.Level = rndLevel
+
+	return pb
+}
+
+func (pb *PokemonBuilder) SetNature(nature Nature) *PokemonBuilder {
+	pb.poke.Nature = nature
+	return pb
+}
+
+func (pb *PokemonBuilder) SetRandomNature() *PokemonBuilder {
+	rndNature := NATURES[rand.IntN(len(NATURES))]
+	pb.poke.Nature = rndNature
+
+	return pb
+}
+
+// NOTE: takes in pointers rather than values even though pokemon struct no longer holds pointers (issues with gob)
+// mainly so i have to change less stuff
+func (pb *PokemonBuilder) SetRandomMoves(possibleMoves []*Move) *PokemonBuilder {
+	var moves [4]Move
+
+	if len(possibleMoves) == 0 {
+		builderLogger().Warn().Msg("This Pokemon was given no available moves to randomize with!")
+		return pb
+	}
+
+	for i := range 4 {
+		move := possibleMoves[rand.IntN(len(possibleMoves))]
+		moves[i] = *move
+	}
+
+	moveNames := lo.Map(moves[:], func(move Move, _ int) string {
+		return move.Name
+	})
+
+	builderLogger().Debug().Strs("Moves", moveNames)
+
+	pb.poke.Moves = moves
+
+	return pb
+}
+
+func (pb *PokemonBuilder) SetRandomAbility(possibleAbilities []Ability) *PokemonBuilder {
+	abilityCount := len(possibleAbilities)
+	if abilityCount == 0 {
+		builderLogger().Warn().Msg("This Pokemon was given no available abilities to randomize with!")
+		return pb
+	}
+
+	hiddenAbility, found := lo.Find(possibleAbilities, func(a Ability) bool {
+		return a.IsHidden
+	})
+	normalAbilities := lo.Filter(possibleAbilities, func(a Ability, _ int) bool {
+		return !a.IsHidden
+	})
+
+	choseHidden := rand.Float64()
+
+	// 1% chance to get a hidden ability randomly
+	if found && choseHidden < 0.01 {
+		pb.poke.Ability = hiddenAbility
+	} else {
+		pb.poke.Ability = normalAbilities[rand.IntN(len(normalAbilities))]
+	}
+
+	return pb
+}
+
+// TODO: SetRandomItem
+
+func (pb *PokemonBuilder) Build() Pokemon {
+	pb.poke.ReCalcStats()
+	builderLogger().Debug().Msg("Building pokemon")
+	return pb.poke
+}
