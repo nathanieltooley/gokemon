d3c4fea6e3c072c4f748362e92a5b2684f2cb274
Nathaniel Tooley <gotablast99@gmail.com>
2025-05-15 00:48:01 -0500

    feat: implement speed-boost

--- tests/abilities_test.go
+++ tests/abilities_test.go
@@ -14,9 +14,11 @@ func init() {
 	global.GlobalInit(os.DirFS("../"), false)
 }
 
+// NOTE: Most ability tests will directly set the ability on a pokemon (probably bulbasaur) rather than choosing a pokemon
+// with that ability for the simple fact that it really doesn't matter. However it may change if for some reason it's important to the ability
 func TestDrizzle(t *testing.T) {
 	pokemon := game.NewPokeBuilder(global.POKEMON.GetPokemonByName("bulbasaur")).Build()
-	enemyPokemon := game.NewPokeBuilder(global.POKEMON.GetPokemonByPokedex(2)).Build()
+	enemyPokemon := getDummyPokemon()
 
 	pokemon.Ability.Name = "drizzle"
 
@@ -26,4 +28,40 @@ func TestDrizzle(t *testing.T) {
 	if gameState.Weather != game.WEATHER_RAIN {
 		t.Fatalf("Expected weather to be rain: got %d", gameState.Weather)
 	}
+}
+
+func TestSpeedBoost(t *testing.T) {
+	pokemon := game.NewPokeBuilder(global.POKEMON.GetPokemonByPokedex(1)).Build()
+	enemyPokemon := getDummyPokemon()
+	pokemon.Ability.Name = "speed-boost"
+
+	gameState := state.NewState([]game.Pokemon{pokemon}, []game.Pokemon{enemyPokemon})
+
+	if gameState.HostPlayer.GetActivePokemon().RawSpeed.Stage != 0 {
+		t.Fatal("test pokemon has started with incorrect speed stage")
+	}
+
+	_ = stateupdater.ProcessTurn(&gameState, []state.Action{})
+
+	pokemonSpeedStage := gameState.HostPlayer.GetActivePokemon().RawSpeed.Stage
+	if pokemonSpeedStage != 1 {
+		t.Fatalf("pokemon's speed stage is incorrect: got %d expected 1", pokemonSpeedStage)
+	}
+
+	enemyPokemonSpeedStage := gameState.ClientPlayer.GetActivePokemon().RawSpeed.Stage
+	if enemyPokemonSpeedStage != 0 {
+		t.Fatal("enemy pokemon's speed stage boosted incorrectly to 1")
+	}
+
+	// Test that pokemon that switch in do not get the speed boost
+	_ = stateupdater.ProcessTurn(&gameState, []state.Action{state.NewSwitchAction(&gameState, state.HOST, 0)})
+
+	pokemonSpeedStage = gameState.HostPlayer.GetActivePokemon().RawSpeed.Stage
+	if pokemonSpeedStage != 1 {
+		t.Fatalf("pokemon's speed stage should have stayed at 1: got %d instead", pokemonSpeedStage)
+	}
+}
+
+func getDummyPokemon() game.Pokemon {
+	return game.NewPokeBuilder(global.POKEMON.GetPokemonByPokedex(2)).Build()
 }
