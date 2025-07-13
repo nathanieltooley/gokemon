4f159713d83ebdf3ffbfe644afe9015fb34f09b2
Nathaniel Tooley <gotablast99@gmail.com>
2025-07-13 12:53:11 -0500

    tests(events): add test case for encoding and decoding events

---
+++ tests/networking_test.go
@@ -0,0 +1,33 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/gob"
+	"reflect"
+	"testing"
+
+	stateCore "github.com/nathanieltooley/gokemon/client/game/state/core"
+	"github.com/nathanieltooley/gokemon/client/networking"
+)
+
+func TestEncodeDecodeEvents(t *testing.T) {
+	events := []stateCore.StateEvent{stateCore.SwitchEvent{PlayerIndex: 10, SwitchIndex: 100}, stateCore.AttackEvent{AttackerId: 20, MoveId: 30}}
+	es := networking.EventSlice{Events: events}
+
+	encodeBuf := bytes.Buffer{}
+	encoder := gob.NewEncoder(&encodeBuf)
+	if err := encoder.Encode(es); err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	decoder := gob.NewDecoder(&encodeBuf)
+	newEs := networking.EventSlice{}
+
+	if err := decoder.Decode(&newEs); err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	if !reflect.DeepEqual(es, newEs) {
+		t.Fatalf("start and end EventSlice values are not equal: %+v != %+v", es, newEs)
+	}
+}
