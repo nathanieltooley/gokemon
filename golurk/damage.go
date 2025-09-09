b4764140ed68c4c9f23055cc12cc20d1dc8c1754
Nathaniel Tooley <gotablast99@gmail.com>
2025-09-08 20:52:55 -0500

    feat(golurk): add snow and hail solar debuff

--- golurk/damage.go
+++ golurk/damage.go
@@ -99,7 +99,7 @@ func Damage(attacker Pokemon, defendent Pokemon, move Move, crit bool, weather i
 		d = uint(float64(d) * 1.5)
 	}
 
-	if weather == WEATHER_SANDSTORM && (move.Name == "solar-beam" || move.Name == "solar-blade") {
+	if (weather == WEATHER_SANDSTORM || weather == WEATHER_HAIL || weather == WEATHER_SNOW) && (move.Name == "solar-beam" || move.Name == "solar-blade") {
 		power = int(float64(power) * 0.5)
 	}
 
